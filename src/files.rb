81d79b9c4b2ff91ded066c254e253f1c230ece5e
Serghei Iakovlev <serghei@phalconphp.com>
2017-04-19 00:35:28 +0300

    Clean Files::configure

--- src/files.rb
+++ src/files.rb
@@ -8,12 +8,12 @@ class Files
   end
 
   def configure
-    if settings.include? 'copy'
-      settings['copy'].each do |file|
-        config.vm.provision 'file' do |f|
-          f.source = File.expand_path(file['from'])
-          f.destination = file['to'].chomp('/') + '/' + file['from'].split('/').last
-        end
+    return unless settings.include? 'copy'
+
+    settings['copy'].each do |file|
+      config.vm.provision 'file' do |f|
+        f.source = File.expand_path(file['from'])
+        f.destination = file['to'].chomp('/') + '/' + file['from'].split('/').last
       end
     end
   end

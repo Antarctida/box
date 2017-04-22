e6fb304c813b37ef2a57359c4de3a81864f24cd6
Serghei Iakovlev <serghei@phalconphp.com>
2017-04-22 16:49:22 +0300

    Cleaned provision

--- src/provision/mongo.sh
+++ src/provision/mongo.sh
@@ -11,15 +11,3 @@
 # through the world-wide-web, please send an email to license@phalconphp.com
 # so that we can send you a copy immediately.
 #
-
-# trace ERR through pipes
-set -o pipefail
-
-# trace ERR through 'time command' and other functions
-set -o errtrace
-
-# set -u : exit the script if you try to use an uninitialised variable
-set -o nounset
-
-# set -e : exit the script if any statement returns a non-true return value
-set -o errexit
--- src/provision/variables.sh
+++ src/provision/variables.sh
@@ -12,17 +12,5 @@
 # so that we can send you a copy immediately.
 #
 
-# trace ERR through pipes
-set -o pipefail
-
-# trace ERR through 'time command' and other functions
-set -o errtrace
-
-# set -u : exit the script if you try to use an uninitialised variable
-set -o nounset
-
-# set -e : exit the script if any statement returns a non-true return value
-set -o errexit
-
 sed -i '/# Phalcon Box environment variable/,+1d' /home/vagrant/.profile
 sed -i '/; Phalcon Box environment variable/,+1d' /etc/php/7.1/fpm/php-fpm.conf
